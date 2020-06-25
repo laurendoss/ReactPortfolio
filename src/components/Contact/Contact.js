3e42f6687d39aa9e9d9fc4ed492a8a956bdc7b8d
Lauren Doss <doss.laurene@gmail.com>
2020-06-25 09:53:51 -0700

    updated css for contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,11 +8,11 @@ return (
     <div id="CardHolder">
     <Card id="ContactCard">
   <Card.Body>
-      <h1>Contact Information</h1>
-      <h5>Phone Number: 408-891-2183</h5>
-      <h5>Email: doss.laurene@gmail.com</h5>
-      <h5>GitHub: <a className="contactLink" href="https://github.com/laurendoss">github.com/laurendoss</a></h5>
-    <h5>LinkedIn: <a className ="contactLink" href="https://www.linkedin.com/in/laurendoss/">linkedin.com/in/laurendoss</a></h5>
+      <h3>Contact Information</h3>
+      <h6>Phone Number: 408-891-2183</h6>
+      <h6>Email: doss.laurene@gmail.com</h6>
+      <h6>GitHub: <a className="contactLink" href="https://github.com/laurendoss">github.com/laurendoss</a></h6>
+    <h6>LinkedIn: <a className ="contactLink" href="https://www.linkedin.com/in/laurendoss/">linkedin.com/in/laurendoss</a></h6>
   </Card.Body>
 </Card>
 </div>
