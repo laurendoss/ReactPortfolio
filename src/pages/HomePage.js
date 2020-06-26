ad634d0251cfa5fc950adf1c28f33eca906d555a
Lauren Doss <doss.laurene@gmail.com>
2020-06-25 18:33:06 -0700

    updated bootstrap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,7 @@
 import React from 'react'; 
-
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import Photo from '../components/Image/Image'
 import AboutMe from '../components/AboutMe/AboutMe'
 import TechCard from '../components/TechCard/TechCard'
@@ -11,10 +13,21 @@ function HomePage () {
 
     return (
       <div className="App">
-  
+        <Container>
+          <Row>
+            <Col md={{ span: 4, offset: 2}}>
         <Photo/>
+        </Col>
+        <Col md={{ span: 4, offset: 2}}>
         <AboutMe/>
+        </Col>
+        </Row>
+        <Row>
+          <Col md={{ span: 8, offset: 2}}>
         <TechCard/>
+        </Col>
+        </Row>
+        </Container>
 
       </div>
     );
